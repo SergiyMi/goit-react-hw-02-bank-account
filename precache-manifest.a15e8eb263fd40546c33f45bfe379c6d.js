e2df6e6cb90eced2b97f6a9d7e5addc1fe3567f6
SergiyMi <bladeneo@i.ua>
2019-11-02 00:04:05 +0200

    Updates

---
+++ precache-manifest.a15e8eb263fd40546c33f45bfe379c6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d1dfddcf65aa4566313ef5f853e9527",
+    "url": "/SergiyMi/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "3f933708963912ad81bc",
+    "url": "/SergiyMi/goit-react-hw-02-bank-account/static/css/main.ddc45a57.chunk.css"
+  },
+  {
+    "revision": "ae40935767148b05330a",
+    "url": "/SergiyMi/goit-react-hw-02-bank-account/static/js/2.6711bee4.chunk.js"
+  },
+  {
+    "revision": "3f933708963912ad81bc",
+    "url": "/SergiyMi/goit-react-hw-02-bank-account/static/js/main.4ef26e06.chunk.js"
+  },
+  {
+    "revision": "3bbd47d704bc38a57865",
+    "url": "/SergiyMi/goit-react-hw-02-bank-account/static/js/runtime-main.c2e90218.js"
+  }
+]);
