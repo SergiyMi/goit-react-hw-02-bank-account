dc1efcc501f324d0ac83a719729f26de06d5eb72
SergiyMi <bladeneo@i.ua>
2019-11-15 15:18:49 +0200

    change state

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import T from 'prop-types';
 import shortid from 'shortid';
 import { ToastContainer, toast } from 'react-toastify';
@@ -10,138 +10,111 @@ import Balance from '../Balance/Balance';
 import TransactionHistory from '../TransactionHistory/TransactionHistory';
 
 export default class Dashboard extends Component {
+  static defaultProps = {
+    id: shortid.generate(),
+    type: '',
+    amount: 0,
+    date: new Date().toLocaleString(),
+    messageNotEnoughMoney:
+      'На счету недостаточно средств для проведения операции!',
+    messageInvalidAmount: 'Введите сумму для проведения операции!',
+  };
 
-    static defaultProps = {
-        id: shortid.generate(),
-        type: '',
-        amount: 0,
-        date: new Date().toLocaleString(),
-        messageNotEnoughMoney: 'На счету недостаточно средств для проведения операции!',
-        messageInvalidAmount: 'Введите сумму для проведения операции!'
-    }
+  static propTypes = {
+    id: T.string,
+    type: T.string,
+    amount: T.number,
+    date: T.string,
+    messageNotEnoughMoney: T.string,
+    messageInvalidAmount: T.string,
+  };
 
-    static propTypes = {
-        id: T.string,
-        type: T.string,
-        amount: T.number,
-        date: T.string,
-        messageNotEnoughMoney: T.string,
-        messageInvalidAmount: T.string
-    }
+  state = {
+    transactions: [],
+    balance: 0,
+  };
 
-    state = {
-        transactions: [],
-        balance: 0,
-        inputAmount: '',
-        type: '',
-        date: new Date().toLocaleString()
-    }
+  notifyInvalid = () =>
+    toast(this.props.messageInvalidAmount, {
+      position: toast.POSITION.TOP_CENTER,
+    });
 
-    notifyInvalid = () => toast(this.props.messageInvalidAmount, {
-        position: toast.POSITION.TOP_CENTER
-      });
+  notifyNotEnough = () =>
+    toast(this.props.messageNotEnoughMoney, {
+      position: toast.POSITION.TOP_CENTER,
+    });
 
-    notifyNotEnough = () => toast(this.props.messageNotEnoughMoney, {
-        position: toast.POSITION.TOP_CENTER
-    });  
+  handleInputText = e => {
+    this.setState({
+      inputAmount: e.currentTarget.value,
+    });
+  };
 
-    handleInputText = (e) => {
-        this.setState(
+  reset = () => {
+    this.setState({
+      inputAmount: '',
+    });
+  };
+
+  handleDeposit = () => {
+    return this.state.inputAmount > 0
+      ? (this.setState(prevState => ({
+          balance: (this.state.balance =
+            prevState.balance + Number.parseInt(this.state.inputAmount)),
+          transactions: (this.state.transactions = [
+            ...this.state.transactions,
             {
-                inputAmount: e.currentTarget.value
-            }
-        );
-    }
+              type: 'deposit',
+              amount: Number.parseInt(this.state.inputAmount),
+              date: new Date().toLocaleString(),
+              id: shortid.generate(),
+            },
+          ]),
+        })),
+        this.reset())
+      : (this.reset(), this.notifyInvalid());
+  };
 
-    reset = () => {
-        this.setState(
+  handleWithdraw = () => {
+    return this.state.inputAmount === '0' ||
+      this.state.inputAmount === '' ||
+      this.state.inputAmount < 0
+      ? (this.notifyInvalid(), this.reset())
+      : this.state.inputAmount >= 0 &&
+        this.state.inputAmount <= this.state.balance
+      ? (this.setState(prevState => ({
+          balance: (this.state.balance =
+            prevState.balance - Number.parseInt(this.state.inputAmount)),
+          transactions: (this.state.transactions = [
+            ...this.state.transactions,
             {
-                inputAmount: ''
-            }
-        )
-    }
+              type: 'withdraw',
+              amount: Number.parseInt(this.state.inputAmount),
+              date: new Date().toLocaleString(),
+              id: shortid.generate(),
+            },
+          ]),
+        })),
+        this.reset())
+      : (this.notifyNotEnough(), this.reset());
+  };
 
-    handleDeposit = () => {
-        return (
-            this.state.inputAmount > 0 ? 
-            (this.setState((prevState) => ({
-                balance: (
-                    this.state.balance = 
-                    prevState.balance + Number.parseInt(this.state.inputAmount)
-                ),
-                transactions: (
-                    this.state.transactions = [
-                        ...this.state.transactions,
-                        {
-                            type: "deposit",
-                            amount: Number.parseInt(this.state.inputAmount),
-                            date: new Date().toLocaleString(),
-                            id: shortid.generate()
-                        }
-                    ]
-                )          
-                })),
-                this.reset() 
-            )
-            : 
-                (
-                    this.reset(),
-                    this.notifyInvalid()
-                )
-        )         
-    }
-
-    handleWithdraw = () => {
-        return (
-            ((this.state.inputAmount === '0') || (this.state.inputAmount === '') || (this.state.inputAmount < 0)) ? 
-                (
-                    this.notifyInvalid(),
-                    this.reset()
-                )
-            :
-            (((this.state.inputAmount >= 0) && (this.state.inputAmount <= this.state.balance)) ?
-            (this.setState((prevState) => ({
-                balance: (
-                    this.state.balance = prevState.balance - Number.parseInt(this.state.inputAmount)
-                ),
-                transactions: (
-                    this.state.transactions = [
-                        ...this.state.transactions,
-                        {
-                            type: "withdraw",
-                            amount: Number.parseInt(this.state.inputAmount),
-                            date: new Date().toLocaleString(),
-                            id: shortid.generate()
-                        }
-                    ]
-                )            
-                })),
-                this.reset()
-            )
-                :
-                    (this.notifyNotEnough(),
-                    this.reset()
-                    ) 
-            )
-        )
-    }
-
-    render() {
-        return (
-            <div className={styles.dashboard}>
-            <Controls inputAmount={this.state.inputAmount} 
-            onChangeInput={this.handleInputText}
-            onDeposit={this.handleDeposit}
-            onWithdraw={this.handleWithdraw}
-            />
-            <Balance 
-            transactions={this.state.transactions}
-            balance={this.state.balance}
-            />
-            <TransactionHistory items={this.state.transactions}/>
-            <ToastContainer/>
-        </div>
-        )
-    }
-
+  render() {
+    return (
+      <div className={styles.dashboard}>
+        <Controls
+          inputAmount={this.state.inputAmount}
+          onChangeInput={this.handleInputText}
+          onDeposit={this.handleDeposit}
+          onWithdraw={this.handleWithdraw}
+        />
+        <Balance
+          transactions={this.state.transactions}
+          balance={this.state.balance}
+        />
+        <TransactionHistory items={this.state.transactions} />
+        <ToastContainer />
+      </div>
+    );
+  }
 }
--- src/TransactionHistory/TransactionHistory.jsx
+++ src/TransactionHistory/TransactionHistory.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 
 import styles from './TransactionHistory.module.css';
 
-const TransactionHistory = ({items}) => {
-    return (
-        <table className={styles.history}>
-            <thead>
-                <tr>
-                    <th>Transaction</th>
-                    <th>Amount</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-              { items.map(item =>(
-                <tr key={item.id}>
-                  <td>{item.type}</td>
-                  <td>{item.amount}$</td>
-                  <td>{item.date}</td>
-                </tr>
-              ))
-              }
-            </tbody>
-        </table>
-    )
-}
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.history}>
+      <thead>
+        <tr>
+          <th>Transaction</th>
+          <th>Amount</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item => (
+          <tr key={item.id}>
+            <td>{item.type}</td>
+            <td>{item.amount}$</td>
+            <td>{item.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 export default TransactionHistory;
